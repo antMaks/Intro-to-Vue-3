02e340a604ace07589ab65659e3050ae4ad7d0e1
antMaks <maksimantonuk74@gmail.com>
2020-10-31 14:03:33 +0200

    examples for vue 3

---
+++ todo-app/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <Card
+      v-for="cat in cats"
+      :id="cat.id"
+      :name="cat.username"
+      :text="cat.company.catchPhrase"
+      :key="cat.id"
+      :website="cat.website"
+    />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import Card from "./components/Card";
+
+export default {
+  name: "App",
+  components: { Card },
+  setup() {
+    const cats = ref([]);
+    const fetchCats = async () => {
+      cats.value = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    };
+    onMounted(fetchCats);
+    return {
+      cats,
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  background: #ecf0f3;
+}
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: space-around;
+  flex-wrap: wrap;
+  padding: 30px 0px;
+}
+</style>
---
+++ todo-app/src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <img :src="imgUrl" alt="cat pic" />
+    <h1>{{ catName }}</h1>
+    <p>
+      <i>{{ text }}</i>
+    </p>
+    <p>
+      <b>{{ website }}</b>
+    </p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "Card",
+  props: {
+    id: String,
+    name: String,
+    text: String,
+    website: String,
+  },
+  setup(props) {
+    const imgUrl = computed(
+      () => `https://robohash.org/${props.id}?set=set4&size=180x180`
+    );
+    const catName = computed(() => props.name.replace(/[_.-]/g, " "));
+    return {
+      imgUrl,
+      catName,
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  width: 400px;
+  height: 380px;
+  position: relative;
+  background: #ecf0f3;
+  margin-bottom: 30px;
+  padding: 30px 5px;
+  border-radius: 32px;
+  text-align: center;
+  cursor: pointer;
+  font-family: "Montserrat", sans-serif;
+  font-size: 22px;
+  font-weight: semibold;
+  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
+    6px 6px 10px rgba(0, 0, 0, 0.2);
+  color: #6f6cde;
+}
+</style>
